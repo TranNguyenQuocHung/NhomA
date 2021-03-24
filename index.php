972167896a6d5027408a6cecc3b2d2ed64f6fd10
unknown <hungpadilla99@gmail.com>
2021-03-24 14:07:13 +0700

    Up ...

--- index.php
+++ index.php
@@ -7,11 +7,43 @@
     <title>Document</title>
 </head>
 <body>
-<form>
-  <label for="fname">Username:</label><br>
-  <input type="text" id="fname" name="fname"><br>
-  <label for="fname">Password:</label><br>
-  <input type="password" id="fname" name="fname"><br>
+<?php
+session_start();
+?>
+         <?php
+            $msg = '';
+            
+            if (isset($_POST['login']) && !empty($_POST['username']) 
+               && !empty($_POST['password'])) {
+				
+               if ($_POST['username'] == 'admin' && 
+                  $_POST['password'] == 'admin') {
+                  
+                  echo 'Đăng nhập thành công';
+               }else {
+                  $msg = 'Đăng nhập thất bại';
+               }
+            }
+         ?>
+      </div> <!-- /container -->
+      
+      <div class = "container">
+      
+         <form class = "form-signin" role = "form" 
+            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
+            ?>" method = "post">
+            <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
+            <input type = "text" class = "form-control" 
+               name = "username" placeholder = "username" 
+               required autofocus></br>
+            <input type = "password" class = "form-control"
+               name = "password" placeholder = "password" required><br>
+            <button class = "btn btn-lg btn-primary btn-block" type = "submit" 
+               name = "login">Login</button>
+         </form>
+		         
+      </div> 
+     
 </form>
 </body>
 </html>
